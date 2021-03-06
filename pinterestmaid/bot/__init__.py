cdf7178a766390f8f4896159b45446ba10707fec
Nachtalb <info@nachtalb.io>
2021-03-06 19:28:42 +0100

    Download images from pinterest and send them to the user

--- pinterestmaid/bot/__init__.py
+++ pinterestmaid/bot/__init__.py
@@ -1,14 +1,46 @@
-import os
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
-
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import logging
+import os
+import re
+import requests
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 
 def download(update: Update, context: CallbackContext):
-    pass
+    results = re.findall('(pin\.it|pinterest\.[a-z]{1,3})\/(pin\/)?([0-9a-z]+)',
+                         update.message.text,
+                         flags=re.IGNORECASE)
+    if not results:
+        update.message.reply_text('No Pinterest URL found')
+        return
+
+    for pinterest_id in [group[2] for group in results]:
+        response = requests.get(f'https://api.pinterest.com/v3/pidgets/pins/info/?pin_ids={pinterest_id}')
+        if response.status_code != 200:
+            update.message.reply_markdown(f'Something went wrong, sry! `{pinterest_id}`')
+            return
+        data = response.json().get('data')[0]
+        image = next(iter(reversed(data.get('images', {}).values())), None)
+        if not image:
+            update.message.reply_markdown(f'Something went wrong, sry! `{pinterest_id}`')
+            return
+        image_url = image['url']
+        reply_markup = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton('Pinterest', f'https://www.pinterest.ch/pin/{pinterest_id}'),
+                InlineKeyboardButton('Source', data['rich_metadata']['url'])
+            ]
+        ])
+
+        update.message.reply_photo(image_url,
+                                   caption='`{}`: {}'.format(
+                                       pinterest_id,
+                                       data['rich_metadata']['title']),
+                                   reply_markup=reply_markup,
+                                   parse_mode=ParseMode.MARKDOWN)
 
 
 def start(update: Update, context: CallbackContext):
@@ -18,6 +50,7 @@ def start(update: Update, context: CallbackContext):
 def main():
     updater = Updater(token=os.environ['TELEGRAM_TOKEN'], use_context=True)
     updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, download))
     updater.start_polling()
     updater.idle()
 
--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
 
     install_requires=[
         'python-telegram-bot',
+        'requests',
     ],
 
     classifiers=[
